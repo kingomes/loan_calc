b8e3a6a5bf46b24bad6a379ec807dd91a3480eae
omare <omaressa13@aol.com>
2021-10-16 01:19:41 -0500

    switch from oop to functional

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var money_1 = require("./money");
-var d = new money_1.money(2, 3, 4, 5);
-console.log(d.getAmmountBorrowed());
+var tslib_1 = require("tslib");
+var prompts_1 = (0, tslib_1.__importDefault)(require("prompts"));
+function calculateLoanMonthly(interest, amountBorrowed, years) {
+    var amountPerPeriod = amountBorrowed * (interest * (Math.pow(1 + interest, years / 12)) / ((Math.pow(1 + interest, years / 12) - 1)));
+    return amountPerPeriod;
+}
+var questions = [
+    {
+        type: 'number',
+        name: 'interest',
+        message: "Enter interest: "
+    },
+    {
+        type: 'number',
+        name: 'amountBorrowed',
+        message: "Enter the amount you wish to borrow: "
+    },
+    {
+        type: 'number',
+        name: 'years',
+        message: "Enter how many years you have to pay back the loan"
+    }
+];
+(function () { return (0, tslib_1.__awaiter)(void 0, void 0, void 0, function () {
+    var response;
+    return (0, tslib_1.__generator)(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4, (0, prompts_1.default)(questions)];
+            case 1:
+                response = _a.sent();
+                console.log(response.interest);
+                return [2];
+        }
+    });
+}); })();
+function getInterest(response) {
+    console.log(response);
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
-import { money } from "./money"
+import prompts from 'prompts';
 
-const d = new money(2, 3, 4, 5)
-console.log(d.getAmmountBorrowed())
+function calculateLoanMonthly(interest : number, amountBorrowed : number, years : number) : number {
+    let amountPerPeriod = amountBorrowed * (interest * (Math.pow(1 + interest, years / 12)) / ((Math.pow(1 + interest, years / 12) - 1)));
+    return amountPerPeriod;
+}
+
+const questions = [
+    {
+        type: 'number',
+        name: 'interest',
+        message: "Enter interest: "
+    },
+    {
+        type: 'number',
+        name: 'amountBorrowed',
+        message: "Enter the amount you wish to borrow: "
+    },
+    {
+        type: 'number',
+        name: 'years',
+        message: "Enter how many years you have to pay back the loan"
+    }
+];
+
+(async() => {
+    const response = await prompts(questions);
+    console.log(response)
+}) ();
+
+function getInterest(response) {
+    console.log(response)
+}
